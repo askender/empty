a057de0a3e2a4b68923043a87fe8e172b34b61b3
askender <askender43@gmail.com>
2021-04-09 02:39:29 +0800

    init 5

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,13 @@
 <template>
-  <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
-      <button
-        v-if="$siteConfig.newsletter.on"
-        class="button is-primary"
-        @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
-      >
-        Subscribe To Newsletter
-      </button>
-    </site-hero>
-    <main-section theme="one-column">
-      <template v-slot:default>
-        <!-- All Posts -->
-        <posts-grid />
-      </template>
-      <template v-slot:sidebar>
-        Nothing here
-      </template>
-    </main-section>
-    <news-letter-form-modal />
-  </div>
+  <main>
+    <section class="self-center flex flex-col flex-1 items-center justify-center">
+      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
+      <h2 class="subtitle text-center">Boilerplate</h2>
+    </section>
+
+    <section class="mt-8">
+      <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
+      <posts post-type="blog" :amount="1" />
+    </section>
+  </main>
 </template>
-
-<script>
-import { mapState } from 'vuex'
-import { setPageData } from '../helper'
-import NewsLetterFormModal from '~/components/NewsLetterFormModal'
-
-export default {
-  name: 'HomePage',
-  head() {
-    return {
-      title: `Home | ${this.$siteConfig.siteName}`
-    }
-  },
-  components: {
-    NewsLetterFormModal
-  },
-  computed: {
-    ...mapState(['title', 'subtitle', 'featureImage'])
-  },
-  fetch({ store, params }) {
-    setPageData(store, { slug: 'home' })
-  }
-}
-</script>
-
-<style>
-.home-page .under-subtitle {
-  border-top: none;
-}
-</style>
