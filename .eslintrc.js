6769f7710159a7c81020b88ef7ad62d3aa3e9d14
askender <askender43@gmail.com>
2021-04-09 00:27:59 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    "no-console": "off",
+    "vue/no-v-html": "off",
+    "vue/html-self-closing": "off",
+    "nuxt/no-timing-in-fetch-data": "off"
+  }
+}
